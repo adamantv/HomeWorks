7bfb97e9fe4a02fe9e2c6c52ccc80de2847c5a77
valeria.golovko <valeria.golovko@megafon.ru>
2021-09-05 23:19:31 +0400

    uncomment a few lines

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         SortService sortService = new SortService();
-//        List<Integer> listForStandartSort = sortService.generateListRandomValues(10000);
-//        System.out.println("Исходный массив");
-//        System.out.println(listForStandartSort);
-//        System.out.println("Сортировка средствами Collections");
-//        long s1 = System.currentTimeMillis();
-//        Collections.sort(listForStandartSort);
-//        System.out.println("Время на cортировкy средствами Collections " + (System.currentTimeMillis() - s1));
-//        System.out.println(listForStandartSort);
+        List<Integer> listForStandartSort = sortService.generateListRandomValues(10000);
+        System.out.println("Исходный массив");
+        System.out.println(listForStandartSort);
+        System.out.println("Сортировка средствами Collections");
+        long s1 = System.currentTimeMillis();
+        Collections.sort(listForStandartSort);
+        System.out.println("Время на cортировкy средствами Collections " + (System.currentTimeMillis() - s1));
+        System.out.println(listForStandartSort);
 
         List<Integer> listForBubbleSort = sortService.generateListRandomValues(10000);
         System.out.println("Исходный массив");
@@ -24,14 +24,14 @@ public class Main {
         System.out.println("Время на cортировкy пузырьком " + (System.currentTimeMillis() - s2));
         System.out.println(listForBubbleSort);
 
-//        List<Integer> listForSelectionSort = sortService.generateListRandomValues(10000);
-//        System.out.println("Исходный массив");
-//        System.out.println(listForSelectionSort);
-//        System.out.println("Сортировка выбором");
-//        long s3 = System.currentTimeMillis();
-//        sortService.selectionSort(listForSelectionSort);
-//        System.out.println("Время на cортировкy выбором " + (System.currentTimeMillis() - s3));
-//        System.out.println(listForSelectionSort);
+        List<Integer> listForSelectionSort = sortService.generateListRandomValues(10000);
+        System.out.println("Исходный массив");
+        System.out.println(listForSelectionSort);
+        System.out.println("Сортировка выбором");
+        long s3 = System.currentTimeMillis();
+        sortService.selectionSort(listForSelectionSort);
+        System.out.println("Время на cортировкy выбором " + (System.currentTimeMillis() - s3));
+        System.out.println(listForSelectionSort);
 
     }
 
