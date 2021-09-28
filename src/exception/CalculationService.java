d7b85f5db4b514ccc1ad441d3cf9e41ed3cee408
valeria.golovko <valeria.golovko@megafon.ru>
2021-09-28 17:32:19 +0400

    fixed example with rethrow

--- src/exception/CalculationService.java
+++ src/exception/CalculationService.java
@@ -1,45 +1,35 @@
 package exception;
 
+import java.util.Arrays;
+
 public class CalculationService {
-    private static final int[] ARRAY_FOR_CALCULATE = {-1,0,1};
     /**
-     * Example for logging few exceptions
+     * Example for rethrow exception
+     *
+     * @param a input number (dividend)
+     * @param b input number (divisor)
+     * @return result of divide
+     */
+    public int divide(int a, int b) throws MyException {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            throw new MyException("Trouble during try to divide " + a + " by " + b);
+        }
+    }
+
+    /**
+     * Example for only logging exception, do nothing
+     *
      * @param number - input parameter
      */
-    public void calculate(int number) {
+    public void addNumberToArray(int[] array, int number, int index) {
         try {
-            ARRAY_FOR_CALCULATE[number-1] = 4/number;
-            System.out.println(ARRAY_FOR_CALCULATE[number]);
-        } catch (ArithmeticException e) {
-            System.out.println("Incorrect arithmetic operation");
-            e.printStackTrace();
+            array[index] = number;
+            System.out.println(Arrays.toString(array));
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Array index is incorrect");
             e.printStackTrace();
-        } catch (Exception e) {
-            System.out.println("Something went wrong");
-            e.printStackTrace();
         }
-    }
-
-    /**
-     * Example for rethrow exception
-     * @param number - input number
-     */
-    public void checkNumberForZero(int number) throws Exception {
-        try {
-            if (number == 0) {
-                System.out.println("Number is zero");
-                throwException();
-            }
-        } catch(Exception e) {
-            System.out.println("Exception thrown from checkNumberForZero() method");
-            throw e;
-        }
-    }
-
-    private void throwException() throws Exception {
-        System.out.println("Need to throw exception");
-        throw new Exception("Exception thrown from throwException() method");
     }
 }
--- src/exception/Main.java
+++ src/exception/Main.java
@@ -3,16 +3,15 @@ package exception;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, MyException {
         InputDataHandler inputDataHandler = new InputDataHandler();
         CalculationService calculationService = new CalculationService();
         String inputString = inputDataHandler.readStringFromKeyboard();
         int number = inputDataHandler.parseString(inputString);
-        try {
-            calculationService.checkNumberForZero(number);
-            calculationService.calculate(number);
-        } catch (Exception e) {
-            System.out.println("Error worker");
-        }
+        int[] array = {-1, 0, 1};
+        int index = array.length;
+        calculationService.addNumberToArray(array, number, index);
+        int result = calculationService.divide(number, args.length);
+        System.out.println(result);
     }
 }
