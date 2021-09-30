ad7e36da93f5b34798c635b11ca8501cfbb42e3b
valeria.golovko <valeria.golovko@megafon.ru>
2021-09-30 22:22:12 +0400

    fixed added name validation

--- src/web/calculateTodayDate.js
+++ src/web/calculateTodayDate.js
@@ -3,7 +3,7 @@
  */
 Date.prototype.toDateInputValue = (function () {
     const local = new Date(this);
-    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    local.setFullYear(this.getFullYear() - 15);
     return local.toJSON().slice(0, 10);
 });
 document.getElementById("datePicker").value = new Date().toDateInputValue();
